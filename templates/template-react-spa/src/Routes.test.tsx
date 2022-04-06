1b07cc2972edbfb38fb5fa1a8484d4525d565973
Lucas Alves <lucascosta.lo77@gmail.com>
2022-04-06 10:41:46 -0300

    fix: Adjust template SPA with new fixes

--- templates/template-react-spa/src/Routes.test.tsx
+++ templates/template-react-spa/src/Routes.test.tsx
@@ -1,13 +1,16 @@
+import React from 'react';
+import { HashRouter } from 'react-router-dom';
+
 import { render, screen, waitFor } from '@testing-library/react';
-import { MemoryRouter } from 'react-router';
+
 import Routes from './Routes';
 
 describe('Router', () => {
   it('should render only the example page route', () => {
     render(
-      <MemoryRouter>
+      <HashRouter>
         <Routes />
-      </MemoryRouter>
+      </HashRouter>,
     );
     waitFor(() => {
       expect(screen.getByText('It works! Translate value')).toBeInTheDocument();
