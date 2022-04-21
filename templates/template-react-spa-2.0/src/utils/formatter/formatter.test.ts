e566e5f6cdc14e783ef735d0d7f0f23345e7aba0
Pedro Leme <32654571+pehleme@users.noreply.github.com>
2022-04-21 13:47:44 -0300

    update each to array template

--- templates/template-react-spa-2.0/src/utils/formatter/formatter.test.ts
+++ templates/template-react-spa-2.0/src/utils/formatter/formatter.test.ts
@@ -11,17 +11,17 @@ describe("formatter", () => {
     const dateMock = new Date("2021-01-01T03:00:00.000Z");
 
     it.each([
-      { lang: "en-US", format: "MMMM dd", isCapitalized: false, formattedDate: "January 01" },
-      { lang: "en-ZA", format: "MMMM dd", isCapitalized: false, formattedDate: "January 01" },
-      { lang: "es-CO", format: "MMMM dd", isCapitalized: false, formattedDate: "Enero 01" },
-      { lang: "es-DO", format: "MMMM dd", isCapitalized: false, formattedDate: "Enero 01" },
-      { lang: "es-EC", format: "MMMM dd", isCapitalized: false, formattedDate: "Enero 01" },
-      { lang: "es-EC", format: "EEE, MMM d" }, isCapitalized: false, formattedDate: "Vie, Enero 1"},
-    { lang: "pt-BR", format: "MMMM dd", isCapitalized: false, formattedDate: "janeiro 01" },
-    { lang: "pt-BR", format: "EEEEEE,  'de' MMMM" }, isCapitalized: true, formattedDate: "Sex, 1 de janeiro"},
+      ["en-US", "MMMM dd", false, "January 01"],
+      ["en-ZA", "MMMM dd", false, "January 01"],
+      ["es-CO", "MMMM dd", false, "Enero 01"],
+      ["es-DO", "MMMM dd", false, "Enero 01"],
+      ["es-EC", "MMMM dd", false, "Enero 01"],
+      ["es-EC", "EEE, MMM d", false, "Vie, Enero 1"],
+      ["pt-BR", "MMMM dd", false, "janeiro 01"],
+      ["pt-BR", "EEEEEE,  'de' MMMM", true, "Sex, 1 de janeiro"],
     ])(
-      "should return the correct formatted date when locale is $lang and format is $format",
-      ({ lang, format, formattedDate, isCapitalized }) => {
+      "should return the correct formatted date when locale is $s and format is $s",
+      (lang, format, isCapitalized, formattedDate) => {
         mockAuthenticationValues(lang);
         const dateOptions = {
           formatStr: format,
@@ -33,83 +33,69 @@ describe("formatter", () => {
     );
   });
 
-describe("number", () => {
-  it.each`
-      lang       | originalNumber | formattedNumber
-      ${"en-US"} | ${1000}        | ${"1,000"}
-      ${"en-ZA"} | ${1000}        | ${"1,000"}
-      ${"es-CO"} | ${1000}        | ${"1.000"}
-      ${"es-DO"} | ${1000}        | ${"1.000"}
-      ${"es-EC"} | ${1000}        | ${"1.000"}
-      ${"pt-BR"} | ${1000}        | ${"1.000"}
-    `(
-    "should return the correct formatted number when locale is $lang and given number is $originalNumber",
-    ({ lang, originalNumber, formattedNumber }) => {
-      mockAuthenticationValues(lang);
-      const expectedFormattedNumber = Formatter.number(originalNumber);
-      expect(expectedFormattedNumber).toEqual(formattedNumber);
-    },
-  );
-});
+  describe("number", () => {
+    it.each([
+      ["en-US", 1000, "1,000"],
+      ["en-ZA", 1000, "1,000"],
+      ["es-CO", 1000, "1.000"],
+      ["es-DO", 1000, "1.000"],
+      ["es-EC", 1000, "1.000"],
+      ["pt-BR", 1000, "1.000"],
+    ])(
+      "should return the correct formatted number when locale is $s and given number is $d",
+      (lang, originalNumber, formattedNumber) => {
+        mockAuthenticationValues(lang);
+        const expectedFormattedNumber = Formatter.number(originalNumber);
+        expect(expectedFormattedNumber).toEqual(formattedNumber);
+      },
+    );
+  });
 
-describe("normalize", () => {
-  it("should return the correct formatted number normalized", () => {
-    const expectedFormattedNumberNormalized = Formatter.normalize({
-      value: 50,
-      min: 0,
-      max: 100,
+  describe("percentage", () => {
+    it.each([
+      ["en-US", 1, false, "1%"],
+      ["en-US", -1, false, "1%"],
+      ["en-US", -1, true, "-1%"],
+      ["en-US", 10.5, false, "10.5%"],
+      ["en-ZA", 10.5, false, "10.5%"],
+      ["es-CO", 10.5, false, "10,5%"],
+      ["es-DO", 10.5, false, "10,5%"],
+      ["es-EC", 10.5, false, "10,5%"],
+      ["pt-BR", 10.5, false, "10,5%"],
+    ])(
+      "should return the correct formatted number when locale is $lang and given number is $originalNumber",
+      (lang, originalNumber, allowNegative, formattedNumber) => {
+        mockAuthenticationValues(lang);
+        const expectedFormattedNumber = Formatter.percentage(
+          originalNumber,
+          allowNegative,
+        );
+        expect(expectedFormattedNumber).toEqual(formattedNumber);
+      },
+    );
+  });
+
+  describe("capitalize", () => {
+    it("should return correct capitalized string", () => {
+      const expectedCapitalizedString = Formatter.capitalize("capitalize");
+      expect(expectedCapitalizedString).toEqual("Capitalize");
     });
-    expect(expectedFormattedNumberNormalized).toEqual(0.5);
   });
-});
 
-describe("percentage", () => {
-  it.each`
-      lang       | originalNumber | allowNegative | formattedNumber
-      ${"en-US"} | ${1}           | ${false}      | ${"1%"}
-      ${"en-US"} | ${-1}          | ${false}      | ${"1%"}
-      ${"en-US"} | ${-1}          | ${true}       | ${"-1%"}
-      ${"en-US"} | ${10.5}        | ${false}      | ${"10.5%"}
-      ${"en-ZA"} | ${10.5}        | ${false}      | ${"10.5%"}
-      ${"es-CO"} | ${10.5}        | ${false}      | ${"10,5%"}
-      ${"es-DO"} | ${10.5}        | ${false}      | ${"10,5%"}
-      ${"es-EC"} | ${10.5}        | ${false}      | ${"10,5%"}
-      ${"pt-BR"} | ${10.5}        | ${false}      | ${"10,5%"}
-    `(
-    "should return the correct formatted number when locale is $lang and given number is $originalNumber",
-    ({ lang, originalNumber, allowNegative, formattedNumber }) => {
-      mockAuthenticationValues(lang);
-      const expectedFormattedNumber = Formatter.percentage(
-        originalNumber,
-        allowNegative,
-      );
-      expect(expectedFormattedNumber).toEqual(formattedNumber);
-    },
-  );
-});
-
-describe("capitalize", () => {
-  it("should return correct capitalized string", () => {
-    const expectedCapitalizedString = Formatter.capitalize("capitalize");
-    expect(expectedCapitalizedString).toEqual("Capitalize");
+  describe("truncateString", () => {
+    const MAX_CHIP_LENGTH = 38;
+    it.each([
+      ["Ex ipsum veniam esse", "Ex ipsum veniam esse"],
+      ["Ex ipsum veniam esse eu voluptate reprehenderit", "Ex ipsum veniam esse eu voluptate repr..."]
+    ])(
+      `should return the correct string when string length is $s.length`,
+      (originalString, truncatedString) => {
+        const expectedTruncatedString = Formatter.truncate(
+          originalString,
+          MAX_CHIP_LENGTH,
+        );
+        expect(expectedTruncatedString).toEqual(truncatedString);
+      },
+    );
   });
-});
-
-describe("truncateString", () => {
-  const MAX_CHIP_LENGTH = 38;
-  it.each`
-      originalString                                       | truncatedString
-      ${"Ex ipsum veniam esse"}                            | ${"Ex ipsum veniam esse"}
-      ${"Ex ipsum veniam esse eu voluptate reprehenderit"} | ${"Ex ipsum veniam esse eu voluptate repr..."}
-    `(
-    `should return the correct string when string length is $originalString.length`,
-    ({ originalString, truncatedString }) => {
-      const expectedTruncatedString = Formatter.truncate(
-        originalString,
-        MAX_CHIP_LENGTH,
-      );
-      expect(expectedTruncatedString).toEqual(truncatedString);
-    },
-  );
-});
 });
