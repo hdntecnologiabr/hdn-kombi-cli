cf30f116374c2f5b43e89a7d67d0cc43fbe92e6d
Pedro Leme <32654571+pehleme@users.noreply.github.com>
2022-04-24 17:56:02 -0300

    Update i18n.ts

--- templates/template-react-spa-2.0/src/i18n/i18n.ts
+++ templates/template-react-spa-2.0/src/i18n/i18n.ts
@@ -1,12 +1,12 @@
 import { initReactI18next } from "react-i18next";
 
-import { use } from "i18next";
+import { Resource, use } from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 
 import enUS from "./locales/enUS/common.json";
 import ptBR from "./locales/ptBR/common.json";
 
-const resources = {
+const resources: Resource = {
   ["en-US"]: {
     translation: enUS,
   },
