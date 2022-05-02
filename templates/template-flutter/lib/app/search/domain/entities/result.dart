bd7c207d7250ee7030d4e8007ba51c41734cfc70
Artur Lansoni <artur.lansoni@hdn.digital>
2022-05-02 10:01:37 -0300

    feat: add equatable

--- templates/template-flutter/lib/app/search/domain/entities/result.dart
+++ templates/template-flutter/lib/app/search/domain/entities/result.dart
@@ -1,13 +1,16 @@
-class Result {
+import "package:equatable/equatable.dart";
+
+class Result extends Equatable {
   const Result({
     required this.image,
-    required this.name,
     required this.nickname,
     required this.url,
   });
 
   final String image;
-  final String name;
   final String nickname;
   final String url;
+
+  @override
+  List<Object> get props => [image, nickname, url];
 }
